b8039f5163350749640c2f08796b204993beed73
az <azou@princeton.edu>
2020-01-13 02:59:00 -0500

    Add files via upload

---
+++ sort_files.py
@@ -0,0 +1,23 @@
+import json
+import os
+import shutil
+
+filenames = []
+with open('occluded_imgs.json') as json_file:
+	data = json.load(json_file)
+	filenames = data['pascal_val2012.json']
+
+directory = 'Annotations'
+newDirectory = 'Annotations2'
+ext = '.jpg'
+
+if not os.path.exists(newDirectory):
+	os.makedirs(newDirectory)
+
+for file in filenames:
+	path = os.path.join(directory,file+ext)
+	if not os.path.isfile(path):
+		print("Error, some file with the filename {} does not exist").format(file)
+	# Move everything
+	os.rename(os.path.join(directory,file+ext), os.path.join(newDirectory, file+ext))
+
